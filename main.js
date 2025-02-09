87e078b71c171a5f9d7d2be6d6807d3b1ce5b134
Pablo gonzalez <pablo.gonzalez@epita.fr>
2025-02-09 11:54:50 +0100

    aezsd

--- main.js
+++ main.js
@@ -23,3 +23,20 @@ buttons.forEach(button => {
         }, delay);
     });
 });
+
+document.querySelectorAll(".list .image img").forEach(image => {
+    image.onclick = () => {
+        const popup = document.querySelector(".popup-image");
+        const popupImg = popup.querySelector("img");
+        
+        popup.style.display = "block";
+        popupImg.src = image.getAttribute("src");
+
+        // Close popup when clicking outside the image
+        popup.onclick = (e) => {
+            if (e.target !== popupImg) {
+                popup.style.display = "none";
+            }
+        };
+    };
+});
